dbaf99244414ef6a7512dd670bb9b16e44f39fa1
aracat <neyachi1@gmail.com>
2015-09-21 01:58:22 -0700

    ui improvement, init script

---
+++ run_eer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ `id -u` -ne 0 ]]; then
+	echo "I think this needs to be run as root"
+fi
+
+curl http://127.0.0.1:5984/ 2>&1 | grep 'Connection refused' > /dev/null
+if [[ $? -eq 0 ]]; then
+	echo "Couch doesn't appear to be running. Start it first"
+	exit 1
+fi
+
+curl http://127.0.0.1:5984/eer | grep 'not_found' > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+	echo "Couch doesn't seem to have the EER database. Making it..."
+	curl -X PUT http://127.0.0.1:5984/eer
+	curl -X PUT http://127.0.0.1:5984/eer/_design/eer -d @app/db.json
+fi
+
+gulp mwm &
+gulp serve:user &
+
+echo "Started EER server processes in the background; 'ps' and 'kill -9 ...' the gulp processes to stop it"
