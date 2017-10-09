8225f39e46cbfad0d0f97d750e6ca8f50364d1d8
aracat <neyachi1@gmail.com>
2017-10-09 00:29:56 -0700

    35 minute labels and print button

--- app/app.js
+++ app/app.js
@@ -220,3 +220,11 @@ var app = angular.module('eer', ['ui.router', 'angular-toArrayFilter', 'edmons',
   ;
 
 })();
+
+function printDiv(divName) {
+  var printContents = document.getElementById(divName).innerHTML;
+  var popupWin = window.open('', '_blank', 'width=300,height=300');
+  popupWin.document.open();
+  popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="eer.min.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
+  popupWin.document.close();
+}
