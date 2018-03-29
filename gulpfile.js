977bc3196c9ed7dd7d468e2bfbb6496b1c7a4ac8
Michael J. Spencer <bigcheesegs@gmail.com>
2018-03-28 23:31:34 -0700

    Upgrade to gulp4.

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,49 @@
 'use strict';
 
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const size = require('gulp-size');
+const useref = require('gulp-useref');
+const gulpif = require('gulp-if');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const csso = require('gulp-csso');
+const minifyHTML = require('gulp-minify-html');
+const jshint = require('gulp-jshint');
+const connect = require('gulp-connect');
 var browserSync = require('browser-sync');
 var del = require('del');
 var child_process = require('child_process');
-    
+
 gulp.task('lint', function() {
   return gulp.src("app/**/*.js")
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('default'));
-});
-
-gulp.task('copy', function() {
-  return gulp.src("assets/**")
-    .pipe(gulp.dest("dist"))
-    .pipe($.size({title: "copy"}));
-});
-
-gulp.task('html', function() {
-  var assets = $.useref.assets({searchPath: "{app,assets}"});
-  
-  return gulp.src("app/**/*.html")
-    .pipe(assets)
-    // minifiy JavaScript.
-    .pipe($.if("*.js", $.uglify()))
-    .pipe($.if("*.css", $.uncss({
-      html: ['app/**/*.html'],
-      ignore: [/.label.*/]
-    })))
-    .pipe($.if("*.css", $.csso()))
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe($.if("*.html", $.minifyHtml()))
-    .pipe(gulp.dest("dist"))
-    .pipe($.size({title: "html"}));
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('media', function() {
   return gulp.src("app/*.wav")
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("dist"))
+    .pipe(size({title: "media"}));
+});
+
+gulp.task('html', function() {
+  return gulp.src("app/**/*.html")
+    .pipe(useref({searchPath: "{app,assets}"}))
+    // minifiy JavaScript.
+    .pipe(gulpif("*.js", uglify()))
+    //.pipe(gulpif("*.css", postcss({
+    //  html: ['app/**/*.html'],
+    //  ignore: [/.label.*/]
+    //})))
+    .pipe(gulpif("*.css", csso()))
+    .pipe(gulpif("*.html", minifyHTML()))
+    .pipe(gulp.dest("dist"))
+    .pipe(size({title: "html"}));
 });
 
 gulp.task('clean', del.bind(null, ['dist/*', '!dist/.git'], {dot: true}));
+
+gulp.task('default', gulp.series('clean', gulp.parallel('lint', 'html', 'media')));
 
 gulp.task('serve', function() {
   browserSync({
@@ -50,18 +51,18 @@ gulp.task('serve', function() {
   });
 });
 
-gulp.task('serve:dist', ['default'], function() {
+gulp.task('serve:dist', gulp.series('default', function() {
   browserSync({
     server: ['dist']
   });
-});
+}));
 
-gulp.task('serve:user', ['default'], function() {
-  $.connect.server({
+gulp.task('serve:user', gulp.series('default', function() {
+  connect.server({
     port: 3000,
     root: 'dist'
   });
-});
+}));
 
 gulp.task('mwm', function() {
   child_process.spawn('python', ['server/max_weight_matching.py'],
@@ -71,4 +72,21 @@ gulp.task('mwm', function() {
     });
 });
 
+function f() {
+gulp.task('lint', function() {
+  return gulp.src("app/**/*.js")
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('default'));
+});
+
+gulp.task('copy', function() {
+  return gulp.src("assets/**")
+    .pipe(gulp.dest("dist"))
+    .pipe(size({title: "copy"}));
+});
+
+
+
+
 gulp.task('default', ['clean', 'lint', 'html', 'media']);
+}
